5f2d68aaf8acf29398f4a6f0281a65099699b292
Emilson Filho <92304613+emilsonfilho@users.noreply.github.com>
2021-11-20 09:44:53 -0300

    JavaScript

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+var a = window.document.getElementById('project1')
+var b = window.document.getElementById('project2')
+var c = window.document.getElementById('project3')
+
+function entrar1() {
+				a.style.width = '300px'
+				a.style.height = '300px'
+				a.style.transitionDuration = '1s'
+				a.style.fontSize = '1.7em'
+				b.style.width = '250px'
+				b.style.height = '250px'
+				b.style.transitionDuration = '1s'
+				b.style.fontSize = '1em'
+				b.style.padding = '0px 10px'
+				c.style.width = '250px'
+				c.style.height = '250px'
+				c.style.transitionDuration = '1s'
+				c.style.fontSize = '1em'
+				c.style.padding = '0px 10px'
+}
+
+function entrar2() {
+				b.style.width = '300px'
+				b.style.height = '300px'
+				b.style.transitionDuration = '1s'
+				b.style.fontSize = '1.5em'
+				a.style.width = '250px'
+				a.style.height = '250px'
+				a.style.transitionDuration = '1s'
+				a.style.fontSize = '1em'
+				a.style.padding = '0px 10px'
+				c.style.width = '250px'
+				c.style.height = '250px'
+				c.style.transitionDuration = '1s'
+				c.style.fontSize = '1em'
+				c.style.padding = '0px 10px'
+}
+
+function entrar3() {
+				c.style.width = '300px'
+				c.style.height = '300px'
+				c.style.transitionDuration = '1s'
+				c.style.fontSize = '1.8em'
+				a.style.width = '250px'
+				a.style.height = '250px'
+				a.style.transitionDuration = '1s'
+				a.style.fontSize = '1em'
+				a.style.padding = '0px 10px'
+				b.style.width = '250px'
+				b.style.height = '250px'
+				b.style.transitionDuration = '1s'
+				b.style.fontSize = '1em'
+				b.style.padding = '0px 10px'
+}
+
+function clique() {
+				alert('Meu email é: emilsonfilhocontato@gmail.com')
+}
